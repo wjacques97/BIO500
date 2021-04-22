2d42a59bcd67ba4ef760775bb4a979cb9031f190
Guillaume Tougas <guillaume.tougas@usherbrooke.ca>
2021-04-22 15:57:57 -0400

    Figure 4 : Comparaison du nombre de cours en ligne et du nombre de cours en présentiel avant et depuis le début de la pandémie

--- Histogramme-4.r
+++ Histogramme-4.r
@@ -1,14 +1,14 @@
 ##Figure 4 : Comparaison du nombre de cours en ligne et du nombre de cours en présentiel avant et depuis le début de la pandémie
 library(ggplot2)
 #Data frame de sp?cification du  moment o? les cours ont eu lieu
-veccourspre<-data.frame(Legende=c(rep("Pre-Covid", 16)))
+veccourspre<-data.frame(Legende=c(rep("Pré-Covid", 16)))
 veccourspost<-data.frame(Legende=c(rep("Post-Covid", 24)))
 
 #Sp?cification des cours de type en ligne ou pr?sentiel
 courspostcov$presentiel[courspostcov$presentiel == "0"] <- "En ligne"
-courspostcov$presentiel[courspostcov$presentiel == "1"] <- "Presentiel"
+courspostcov$presentiel[courspostcov$presentiel == "1"] <- "Présentiel"
 coursprecov$presentiel[coursprecov$presentiel == "0"] <- "En ligne"
-coursprecov$presentiel[coursprecov$presentiel == "1"] <- "Presentiel"
+coursprecov$presentiel[coursprecov$presentiel == "1"] <- "Présentiel"
 
 #Jonction des cours avec le moment o? ils ont eu lieu
 courspostcov1<-cbind(courspostcov, veccourspost)
@@ -20,8 +20,19 @@ coursprepost<-rbind(courspostcov1, coursprecov1)
 #Graphique du type de cours en fonction du moment o? ils ont eu lieu
 p7<-ggplot(coursprepost, aes(presentiel, fill=Legende))+
   geom_histogram(position="dodge", stat="count", binwidth=0.2)+
-  labs(x="Type de cours",
-       y="Nombre de cours de chaque type",
-       title="Comparaison du nombre de cours en ligne et presentiel pre- et post-Covid")+
+  labs(x="Catégorie de cours",
+       y="Nombre de cours de chaque catégorie",
+       title="Comparaison du nombre de cours en ligne et présentiel avant et depuis le début de la pandémie")+
   theme(plot.title = element_text(hjust = 0.5))
+#Visualiser le graphique
 p7
+#Enregistrement de la figure 4 en pdf
+pdf("Histogramme4.pdf", height=10, width=20)
+ggplot(coursprepost, aes(presentiel, fill=Legende))+
+  geom_histogram(position="dodge", stat="count", binwidth=0.2)+
+  labs(x="Catégorie de cours",
+       y="Nombre de cours de chaque catégorie",
+       title="Comparaison du nombre de cours en ligne et présentiel avant et depuis le début de la pandémie")+
+  theme(axis.text=element_text(size=15),
+        axis.title=element_text(size=15),legend.title = element_text(size=15),legend.text = element_text(size=14), plot.title = element_text(hjust = 0.5, size=20))
+dev.off()
