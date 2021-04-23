545558257fa39492612efbb139b142cdce65aca7
Guillaume Tougas <guillaume.tougas@usherbrooke.ca>
2021-04-23 18:54:10 -0400

    Fichier pour MacOS. Pas de bug avec les accents. Script servant à modifier et corriger les données dans les documents csv de chaque équipe et à les combiner ensemble.

--- BIO500_modifications_donnees_sans_bug.R
+++ BIO500_modifications_donnees_sans_bug.R
@@ -19,6 +19,7 @@ Merielnoeuds<-read.table("Meriel_etal_noeuds.csv", header=T, sep=";")
 Teamdefeunoeuds<-read.table("Teamdefeu_noeuds.csv", header=T, sep=";",stringsAsFactors = F)
 Vachonnoeuds<-read.table("Vachon_etal_noeuds.csv", header=T, sep=";")
 
+
 #Standardisation des noms de colonnes
 colnames(Merielcollaborations)[colnames(Merielcollaborations) %in% c("etudiant1", "session")] <- c("etudiant1", "date")
 colnames(Teamdefeucollaborations)[colnames(Teamdefeucollaborations) %in% c("etudiant1")] <- c("etudiant1")
